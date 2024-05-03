f79a1fc518dbf32ac52bb8adb5394cc5302a84a1
RGGH <pisscompany@gmail.com>
2024-05-03 22:45:41 +0100

    first commit

---
+++ tests/test_bcd.py
@@ -0,0 +1,9 @@
+from unittest.mock import patch
+from app.fetch_www import parse
+
+
+@patch("app.fetch_www")
+def test_parse_from_fetch_net(mock_get):
+    """meh"""
+    mock_get.return_value = "def"
+    assert parse() == "abc123"
