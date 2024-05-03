7043b19c70e85908fea4b9459ba1682ace89635f
Red and Green <62441426+RGGH@users.noreply.github.com>
2024-05-03 23:13:07 +0100

    Update test_bcd.py

--- tests/test_bcd.py
+++ tests/test_bcd.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from app.fetch_www import parse
 
 
-@patch("app.fetch_www")
+@patch("..app.fetch_www")
 def test_parse_from_fetch_net(mock_get):
     """meh"""
     mock_get.return_value = "def"
